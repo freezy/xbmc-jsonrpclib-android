2e0c253ce7bea7c5ee1cea8e6bdd4de6102e8b11
freezy <freezy@xbmc.org>
2012-10-08 00:21:22 +0200

    Added: Main launches code generation.

--- src/org/xbmc/android/jsonrpc/generator/Introspect.java
+++ src/org/xbmc/android/jsonrpc/generator/Introspect.java
@@ -2,19 +2,20 @@ package org.xbmc.android.jsonrpc.generator;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.Version;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.module.SimpleModule;
-import org.xbmc.android.jsonrpc.generator.introspect.Param;
+import org.xbmc.android.jsonrpc.generator.controller.TypeController;
 import org.xbmc.android.jsonrpc.generator.introspect.Response;
 import org.xbmc.android.jsonrpc.generator.introspect.Result;
 import org.xbmc.android.jsonrpc.generator.introspect.wrapper.AdditionalPropertiesWrapper;
 import org.xbmc.android.jsonrpc.generator.introspect.wrapper.ExtendsWrapper;
 import org.xbmc.android.jsonrpc.generator.introspect.wrapper.TypeWrapper;
+import org.xbmc.android.jsonrpc.generator.model.Namespace;
+import org.xbmc.android.jsonrpc.generator.view.NamespaceView;
 import org.xbmc.android.jsonrpc.jackson.AdditionalPropertiesDeserializer;
 import org.xbmc.android.jsonrpc.jackson.ExtendsDeserializer;
 import org.xbmc.android.jsonrpc.jackson.TypeDeserializer;
@@ -30,21 +31,24 @@ public class Introspect {
 		module.addDeserializer(AdditionalPropertiesWrapper.class, new AdditionalPropertiesDeserializer());
 		
 		OBJECT_MAPPER.registerModule(module);
-//		OBJECT_MAPPER.enableDefaultTyping(); // default to using DefaultTyping.OBJECT_AND_NON_CONCRETE
-//		OBJECT_MAPPER.enableDefaultTyping(ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT);
 	}
 	
 	public static void main(String[] args) {
 		try {
 			
-		    //final Map<String,Object> userData = mapper.readValue(new File("R:/phplib/xbmc-introspect.json"), Map.class);
 		    final Response response = OBJECT_MAPPER.readValue(new File("R:/phplib/xbmc-introspect.json"), Response.class);
 		    final Result result = response.getResult();
 			
-			final List<Param> params = result.getMethods().get("Addons.ExecuteAddon").getParams();
-			for (Param param : params) {
-				System.out.println(param);
-			}
+		    for (String name : result.getTypes().keySet()) {
+		    	final TypeController controller = new TypeController(name, result.getTypes().get(name));
+		    	controller.register();
+		    }
+		    
+		    for (Namespace ns : Namespace.getAll()) {
+		    	final NamespaceView view = new NamespaceView(ns);
+		    	System.out.println(view.render());
+		    }
+		    
 			System.out.println("Done!");
 			
 		} catch (JsonParseException e) {
