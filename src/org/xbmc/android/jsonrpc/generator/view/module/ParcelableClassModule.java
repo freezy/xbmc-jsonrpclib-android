0199e5dad39901656ba255a44c347090d616f050
freezy <freezy@xbmc.org>
2012-10-18 21:38:36 +0200

    Added: Codegen for parcelization method.

--- src/org/xbmc/android/jsonrpc/generator/view/module/ParcelableClassModule.java
+++ src/org/xbmc/android/jsonrpc/generator/view/module/ParcelableClassModule.java
@@ -24,6 +24,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.xbmc.android.jsonrpc.generator.model.Klass;
+import org.xbmc.android.jsonrpc.generator.model.Member;
 import org.xbmc.android.jsonrpc.generator.model.Namespace;
 import org.xbmc.android.jsonrpc.generator.view.AbstractView;
 
@@ -39,7 +40,7 @@ public class ParcelableClassModule extends AbstractView implements IClassModule 
 	public void render(StringBuilder sb, Namespace ns, Klass klass, int idt) {
 		
 		// writeToParcel
-		renderWriteToParcel(sb, klass, idt);
+		renderWriteToParcel(sb, ns, klass, idt);
 		
 		// describeContents
 		renderDescribeContents(sb, idt);
@@ -53,13 +54,70 @@ public class ParcelableClassModule extends AbstractView implements IClassModule 
 		return imports;
 	}
 	
-	private void renderWriteToParcel(StringBuilder sb, Klass klass, int idt) {
+	private void renderWriteToParcel(StringBuilder sb, Namespace ns, Klass klass, int idt) {
 		final String indent = getIndent(idt);
 		
-		// TODO
 		sb.append(indent).append("@Override\n");
 		sb.append(indent).append("public void writeToParcel(Parcel parcel, int flags) {\n");
+		if (klass.doesExtend()) {
+			sb.append(indent).append("	super.writeToParcel(parcel, flags);\n");
+		}
+		for (Member member : klass.getMembers()) {
+			renderWriteToParcel(sb, ns, member, idt + 1);
+		}
 		sb.append(indent).append("}\n");
+	}
+	
+	private void renderWriteToParcel(StringBuilder sb, Namespace ns, Member member, int idt) {
+		final String indent = getIndent(idt);
+		
+		if (member.isEnum()) {
+			// TODO
+			sb.append(indent).append("/* enum: ").append(member.getName()).append(" */\n");
+		} else {
+			
+			final Klass klass = member.getType();
+			if (klass.isArray()) {
+				final Klass arrayType = klass.getArrayType();
+				
+				// like: parcel.writeInt(genre.size());
+				sb.append(indent).append("parcel.writeInt(");
+				sb.append(member.getName());
+				sb.append(".size());\n");
+				
+				// like: for (String item : genre) {
+				sb.append(indent).append("for (");
+				sb.append(getClassReference(ns, arrayType));
+				sb.append(" item : ");
+				sb.append(member.getName());
+				sb.append(") {\n");
+				
+				// like: parcel.writeValue(item);
+				sb.append(indent).append("	parcel.");
+				if (arrayType.isNative()) {
+					sb.append("writeValue(item);\n");
+				} else {
+					sb.append("writeParcelable(item, flags);\n");
+				}
+				
+				sb.append(indent).append("}\n");
+				
+			} else {
+				// like: parcel.writeInt(muted ? 1 : 0);
+				if (klass.isNative() && klass.getName().equals("boolean")) {
+					sb.append(indent).append("parcel.writeInt(");
+					sb.append(member.getName());
+					sb.append(" ? 1 : 0);\n");
+				
+				// like: parcel.writeValue(data);
+				} else {
+					sb.append(indent).append("parcel.writeValue(");
+					sb.append(member.getName());
+					sb.append(");\n");
+				}
+			}
+			
+		}
 	}
 	
 	
