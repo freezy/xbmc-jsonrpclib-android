6151ce6933ef89c07743941ba47333ef874769f1
freezy <freezy@xbmc.org>
2014-02-02 23:31:45 +0100

    Added missing ORDER to cast in introspect (going upstream soon).

--- xbmc-jsonrpclib/src/main/java/org/xbmc/android/jsonrpc/io/JsonHandler.java
+++ xbmc-jsonrpclib/src/main/java/org/xbmc/android/jsonrpc/io/JsonHandler.java
@@ -21,29 +21,27 @@
 
 package org.xbmc.android.jsonrpc.io;
 
-import java.io.IOException;
-
-import org.codehaus.jackson.JsonNode;
-import org.json.JSONException;
-
 import android.content.ContentProvider;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
+import org.codehaus.jackson.JsonNode;
+
+import java.io.IOException;
 
 /**
  * Abstract class that handles reading and parsing an JSON-serialized API
  * response into a set of ContentProviderOperation. It catches
- * recoverable network exceptions and re-throws them as {@link ApiException}. 
+ * recoverable network exceptions and re-throws them as {@link ApiException}.
  * Any local {@link ContentProvider} exceptions are considered unrecoverable.
  * <p>
  * This class is only designed to handle simple one-way synchronization.
  * <p>
  * This class was closely inspired by Google's official iosched app, see
  * http://code.google.com/p/iosched/
- * 
+ *
  * @author freezy <freezy@xbmc.org>
  */
 public abstract class JsonHandler implements Parcelable {
@@ -60,21 +58,21 @@ public abstract class JsonHandler implements Parcelable {
 	 * Parse the given HTTP response body, turning into a series of
 	 * ContentProviderOperation that are immediately applied using the
 	 * given {@link ContentResolver}.
-	 * 
-	 * @param response HTTP response body
+	 *
+	 * @param result HTTP response body
 	 * @param resolver Content resolver
 	 * @throws ApiException Re-thrown errors
 	 */
 	public void applyResult(JsonNode result, ContentResolver resolver) throws ApiException {
 		try {
 			final long start = System.currentTimeMillis();
-			
+
 			if (result == null) {
 				// TODO handle empty response correctly.
 				Log.w(TAG, "Empty response. DEFINE what to do, ignoring now.");
-				
+
 			} else {
-				
+
 				final ContentValues[] newBatch = parse(result, resolver);
 				Log.i(TAG, "Starting to execute " + newBatch.length + " batches..");
 				insert(resolver, newBatch);
@@ -82,22 +80,23 @@ public abstract class JsonHandler implements Parcelable {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		} catch (ClassCastException e) {
+			Log.e(TAG, "Casting problem: " + e.getMessage(), e);
+		}
 	}
 
 	/**
 	 * Parse the HTTP body's de-serialized {@link JsonNode}, returning a set
 	 * of ContentProviderOperation that will bring the
 	 * {@link ContentProvider} into sync with the parsed data.
-	 * 
+	 *
 	 * @param result HTTP body de-serialized
 	 * @param resolver Content resolver
-	 * @return
-	 * @throws JSONException
 	 * @throws IOException
+	 * @return Values to insert into the database
 	 */
 	protected abstract ContentValues[] parse(JsonNode result, ContentResolver resolver) throws IOException;
-	
+
 	protected abstract void insert(ContentResolver resolver, ContentValues[] batch);
 
 	@Override
